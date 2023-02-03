06a1ff87dfeaf29f22cae5f5f575c7c0da517ac8
Cody <cshaner04@gmail.com>
2023-02-03 10:06:32 -0600

    normalize test

---
+++ crawl.test.js
@@ -0,0 +1,32 @@
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
+const { test, expect } = require("@jest/globals");
+
+test("normalizeURL Strip https", () => {
+  const input = "https://blog.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL Strip http", () => {
+  const input = "http://blog.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL Strip Trailing Slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL capitals", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML", () => {});
